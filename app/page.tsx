fcda3f6fa4dcc9a4713effaedaf1560bf40ee9a7
Amani Kaneza Fred <95358548+amankanz@users.noreply.github.com>
2024-12-29 18:12:36 +0300

    home-ft/testimonials-section: work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 import Main from "@/components/Main";
+import TestimonialsSection from "@/components/TestimonialsSection";
 
 export default function Home() {
   return (
     <Main>
       <Hero />
+      <TestimonialsSection />
     </Main>
   );
 }
