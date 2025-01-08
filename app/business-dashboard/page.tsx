24e3278c7e9b73ebb647a61d00a4da36fa969aed
Amani Kaneza Fred <95358548+amankanz@users.noreply.github.com>
2025-01-08 18:11:55 +0300

    ft/signup-login: Customize the dashboards with message under development

--- app/business-dashboard/page.tsx
+++ app/business-dashboard/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function BusinessDashboard() {
-  return <div>BusinessDashboard</div>;
+  return (
+    <div className="w-full max-w-5xl mx-auto flex flex-col items-center justify-center">
+      <h1 className="text-2xl mt-5 font-medium">Business Dashboard</h1>
+      <p className="text-red-400">
+        <span className="underline underline-offset-2">Under Development </span>
+        🚧 🏗 👷‍♂️
+      </p>
+    </div>
+  );
 }
 
 export default BusinessDashboard;
